85078cf2cc3564c663f7f3d560f6018d86b514fb
David Lindahl <davidalindahl@gmail.com>
2017-11-19 13:06:18 -0800

    updated js file to remove the loop and update descriptions

--- public/js/projectsModel.js
+++ public/js/projectsModel.js
@@ -4,29 +4,29 @@ let $sourceHTML = $('#projects-template').html();
 let $compiledTemplate = Handlebars.compile($sourceHTML);
 var $newHTML = $($compiledTemplate({
   project: [
-    {
-      title: 'the loop network | website & app',
-      url: `http://theloopnetwork.io`,
-      img: `assets/imgs/mockup.png`,
-      projectDescription: `we built an online network for colleges to impliment that keeps their students updated on campus events and news. It is currently being deployed and used by over 2,000 students.`
-    },
+    // {
+    //   title: 'the loop network | website & app',
+    //   url: `http://theloopnetwork.io`,
+    //   img: `assets/imgs/mockup.png`,
+    //   projectDescription: `we built an online network for colleges to impliment that keeps their students updated on campus events and news. It is currently being deployed and used by over 2,000 students.`
+    // },
     {
       title: 'posy rae | website',
       url: `http://posyrae.com`,
       img: `assets/imgs/mockup.png`,
-      projectDescription: `we assisted with a website redesign and e-commerce build out for very talented paper florist.`
+      projectDescription: `we spearheaded a website redesign on squarespace for this very talented paper florist.`
     },
     {
       title: 'moving forward leadership | website',
       url: `http://movingforwardleadership.com`,
       img: `assets/imgs/mockup.png`,
-      projectDescription: `for scott of the moving forward leadership blog we designed and developed a new website adding functionality to store podcasts, show notes and articles he wrote.`
+      projectDescription: `we designed and developed a new website with a clean, modern visual appearance and added functionality to store podcasts, show notes for podcasts and leadership articles linked in from medium.com.`
     },
     {
       title: 'fraction calculator | ios app',
       url: `https://itunes.apple.com/us/app/fraction-calculator-pro/id398107543`,
       img: `assets/imgs/mockup.png`,
-      projectDescription: 'this app was the highest rated fraction calculator on the app store for nearly ten years. '
+      projectDescription: 'we designed and developed an ios app for calculating fractions.'
     }
   ]
 }));
