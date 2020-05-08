80e19102d570c21fcc531d0d42f9a087129e8f36
David Lindahl <davidalindahl@gmail.com>
2020-05-08 08:13:09 -0700

    added ledgestone hoa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,16 @@
 <template>
-<div class="flex flex-col max-w-screen sm:py-8 md:py-12 h-full justify-around">
+<div class="flex flex-col justify-around h-full max-w-screen sm:py-8 md:py-12">
 
-  <div class="container mx-auto px-4 sm:px-12 leading-none max-w-3xl flex justify-center">
-    <h1 class="text-2xl md:text-4xl font-black">
-        <a class="flex border-0 hover:opacity-75 items-center text-center" href="http://lindahlstudios.com">
+  <div class="container flex justify-center max-w-3xl px-4 mx-auto leading-none sm:px-12">
+    <h1 class="text-2xl font-black md:text-4xl">
+        <a class="flex items-center text-center border-0 hover:opacity-75" href="http://lindahlstudios.com">
             <img class="w-24 mr-8" alt="our logo, you shouldnt be seeing this" src="@/assets/lindahlstudios-final-bigger-subtitle.png">
         {{ msg }}
         </a>
     </h1>
     </div>
 
-    <div class="container mx-auto px-4 sm:px-12 leading-none max-w-3xl">
+    <div class="container max-w-3xl px-4 mx-auto leading-none sm:px-12">
         <p>
             - a dynamic duo of two brothers making epic things
         </p>
@@ -19,10 +19,10 @@
         </p>
     </div>
 
-    <div class="container mx-auto px-4 sm:px-12 leading-none max-w-3xl">
+    <div class="container max-w-3xl px-4 mx-auto leading-none sm:px-12">
         <div class="my-3">
-            <h3 class="font-bold uppercase font-sans mb-3">things we love using:</h3>
-            <ul class="flex flex-wrap justify-between list-none p-0">
+            <h3 class="mb-3 font-sans font-bold uppercase">things we love using:</h3>
+            <ul class="flex flex-wrap justify-between p-0 list-none">
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://statamic.com/" target="_blank" rel="noopener">statamic</a></li>
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://usefathom.com/ref/SIUVYE" target="_blank" rel="noopener">fathom analytics</a></li>
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://vuejs.org/" target="_blank" rel="noopener">vue.js</a></li>
@@ -33,8 +33,8 @@
             </ul>
         </div>
         <div class="my-3">
-            <h3 class="font-bold uppercase font-sans mb-3">things we are working on:</h3>
-            <ul class="flex flex-wrap justify-between list-none p-0">
+            <h3 class="mb-3 font-sans font-bold uppercase">things we are working on:</h3>
+            <ul class="flex flex-wrap justify-between p-0 list-none">
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://rainierwatch.com" target="_blank" rel="noopener">rainier watch</a></li>
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://madewithspark.com" target="_blank" rel="noopener">made with spark</a></li>
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://seattle2go.com" target="_blank" rel="noopener">seattle2go</a></li>
@@ -42,17 +42,18 @@
 
         </div>
         <div class="my-3">
-            <h3 class="font-bold uppercase font-sans mb-3">clients we've helped:</h3>
-            <ul class="flex flex-wrap justify-between list-none p-0">
+            <h3 class="mb-3 font-sans font-bold uppercase">clients we've helped:</h3>
+            <ul class="flex flex-wrap justify-between p-0 list-none">
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="http://movingforwardleadership.com/" target="_blank" rel="noopener">moving forward leadership</a></li>
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="http://seattlebybike.com/" target="_blank" rel="noopener">seattle by bike</a></li>
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="http://posyrae.com/" target="_blank" rel="noopener">posyrae</a></li>
+            <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://ledgestoneankenyhoa.com/" target="_blank" rel="noopener">ledgestone ankeny hoa</a></li>
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="http://valerieslaybaugh.com/" target="_blank" rel="noopener">valerie counseling</a></li>
             </ul>
         </div>
         <div class="my-3">
-            <h3 class="font-bold uppercase font-sans mb-3">our barely used social media:</h3>
-            <ul class="flex flex-wrap justify-between list-none p-0">
+            <h3 class="mb-3 font-sans font-bold uppercase">our barely used social media:</h3>
+            <ul class="flex flex-wrap justify-between p-0 list-none">
             <li class="inline-block mt-0 mb-2 mr-4"><a class="hover:opacity-75" href="https://twitter.com/lindahlstudios" target="_blank" rel="noopener">the twitter</a></li>
             </ul>
         </div>
